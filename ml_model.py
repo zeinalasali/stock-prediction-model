42f91d17740b3a17e767a73981acefbdcc8bfb6f
zeinalasali <zainalasali2005@gmail.com>
2025-12-30 19:52:25 -0500

    Initial commit

---
+++ ml_model.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import seaborn as sns
+import os
+from datetime import datetime
+
+import warnings
+warnings.filterwarnings("ignore")
+
